e82b1bb8fe36bef644f4ad8c97c5613db7a26fd1
AsriFox <asrifox@yandex.ru>
2024-05-27 20:54:27 +0300

    Added mid-line typing and erasing

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,123 @@ use crossterm::{
     ExecutableCommand,
 };
 
+enum EditorState {
+    CursorMode,
+    InsertMode { after: String },
+}
+
+impl EditorState
+{
+    fn split_buf(&self, buf: &mut String) -> io::Result<String> {
+        let after = match self {
+            Self::CursorMode => {
+                let (col, _) = cursor::position()?;
+                buf.split_off((col as usize).min(buf.len()))
+            }
+            Self::InsertMode { after } => after.to_string(),
+        };
+        Ok(after)
+    }
+
+    pub fn print<W>(self, w: &mut W, buf: &mut String, c: char) -> io::Result<EditorState> 
+    where W: io::Write,
+    {
+        let after = self.split_buf(buf)?;
+        buf.push(c);
+        if after.len() > 0 {
+            execute!(
+                w,
+                style::Print(c),
+                style::Print(&after),
+                cursor::MoveLeft(after.len() as u16),
+            )?;
+        } else {
+            w.execute(style::Print(c))?;
+        }
+        Ok(Self::InsertMode { after })
+    }
+
+    pub fn erase_left<W>(self, w: &mut W, buf: &mut String) -> io::Result<EditorState>
+    where W: io::Write,
+    {
+        let after = self.split_buf(buf)?;
+        buf.pop();
+        if after.len() > 0 {
+            execute!(
+                w,
+                cursor::MoveLeft(1),
+                style::Print(&after),
+                style::Print(' '),
+                cursor::MoveLeft(after.len() as u16 + 1),
+            )?;
+        } else {
+            execute!(
+                w,
+                cursor::MoveLeft(1),
+                style::Print(' '),
+                cursor::MoveLeft(1),
+            )?;
+        }
+        Ok(Self::InsertMode { after })
+    }
+
+    pub fn erase_right<W>(self, w: &mut W, buf: &mut String) -> io::Result<EditorState>
+    where W: io::Write,
+    {
+        let after = match self {
+            Self::CursorMode => self.split_buf(buf)?,
+            Self::InsertMode { after } => after,
+        };
+        if after.len() == 0 {
+            return Ok(Self::InsertMode { after });
+        }
+        let (_, after) = after.split_at(1);
+        execute!(
+            w,
+            style::Print(after),
+            style::Print(' '),
+            cursor::MoveLeft(after.len() as u16 + 1),
+        )?;
+        Ok(Self::InsertMode { after: String::from(after) })
+    }
+
+    pub fn cursor_mode(self, buf: &mut String) -> io::Result<EditorState> {
+        match self {
+            Self::CursorMode => {}
+            Self::InsertMode { after } => {
+                buf.push_str(&after);
+            }
+        }
+        Ok(Self::CursorMode)
+    }
+}
+
 fn typing_synced<W>(w: &mut W, buf: &mut String) -> io::Result<KeyCode>
 where W: io::Write,
 {
     w.execute(cursor::MoveToColumn(buf.len() as u16))?;
+    let mut state = EditorState::CursorMode;
     loop {
-        match read_char()? {
-            KeyCode::Char(c) => {
-                buf.push(c);
-                w.execute(style::Print(c))?;
+        state = match read_char()? {
+            KeyCode::Char(c) => state.print(w, buf, c)?,
+            KeyCode::Backspace => state.erase_left(w, buf)?,
+            KeyCode::Delete => state.erase_right(w, buf)?,
+            KeyCode::Left => {
+                w.execute(cursor::MoveLeft(1))?;
+                state.cursor_mode(buf)?
             }
-            KeyCode::Backspace => {
-                buf.pop();
-                execute!(
-                    w,
-                    cursor::MoveLeft(1),
-                    style::Print(' '),
-                    cursor::MoveLeft(1),
-                )?;
+            KeyCode::Right => {
+                let new_state = state.cursor_mode(buf)?;
+                if (cursor::position()?.0 as usize) < buf.len() {
+                    w.execute(cursor::MoveRight(1))?;
+                }
+                new_state
             }
             KeyCode::Up => return Ok(KeyCode::Up),
             KeyCode::Down => return Ok(KeyCode::Down),
             KeyCode::Enter => return Ok(KeyCode::Enter),
             KeyCode::Esc => return Ok(KeyCode::Esc),
-            _ => {}
+            _ => continue,
         };
     }
 }
@@ -44,6 +137,7 @@ where W: io::Write,
         w,
         style::ResetColor,
         terminal::Clear(terminal::ClearType::All),
+        cursor::SetCursorStyle::BlinkingBar,
         cursor::MoveTo(1, 1),
     )?;
 
@@ -80,6 +174,7 @@ where W: io::Write,
         w,
         style::ResetColor,
         terminal::LeaveAlternateScreen,
+        cursor::SetCursorStyle::DefaultUserShape,
     )?;
 
     terminal::disable_raw_mode()?;
