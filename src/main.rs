bc2c2669f18b4be9e42ae804690fe4c29a48f34b
AsriFox <asrifox@yandex.ru>
2024-05-27 10:02:13 +0300

    Added line typing

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,75 @@ use std::io;
 
 use crossterm::{
     execute,
-    queue,
-    style,
-    event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
-    terminal,
-    cursor,
-    Command,
+    style, terminal, cursor,
+    event::KeyCode,
+    ExecutableCommand,
 };
 
-const TEXT: &str = r#"Interactive test
-
-press 'q' to exit
-"#;
+fn typing_synced<W>(w: &mut W, buf: &mut String) -> io::Result<KeyCode>
+where W: io::Write,
+{
+    w.execute(cursor::MoveToColumn(buf.len() as u16))?;
+    loop {
+        match read_char()? {
+            KeyCode::Char(c) => {
+                buf.push(c);
+                w.execute(style::Print(c))?;
+            }
+            KeyCode::Backspace => {
+                buf.pop();
+                execute!(
+                    w,
+                    cursor::MoveLeft(1),
+                    style::Print(' '),
+                    cursor::MoveLeft(1),
+                )?;
+            }
+            KeyCode::Up => return Ok(KeyCode::Up),
+            KeyCode::Down => return Ok(KeyCode::Down),
+            KeyCode::Enter => return Ok(KeyCode::Enter),
+            KeyCode::Esc => return Ok(KeyCode::Esc),
+            _ => {}
+        };
+    }
+}
 
 fn run<W>(w: &mut W) -> io::Result<()>
-where W: io::Write, {
+where W: io::Write,
+{
     execute!(w, terminal::EnterAlternateScreen)?;
     terminal::enable_raw_mode()?;
+    execute!(
+        w,
+        style::ResetColor,
+        terminal::Clear(terminal::ClearType::All),
+        cursor::MoveTo(1, 1),
+    )?;
+
+    let mut buffer = vec![String::new()];
+    let mut i = 0;
+
     loop {
-        queue!(
-            w,
-            style::ResetColor,
-            terminal::Clear(terminal::ClearType::All),
-            cursor::Hide,
-            cursor::MoveTo(1, 1),
-        )?;
-
-        for line in TEXT.split('\n') {
-            queue!(w, style::Print(line), cursor::MoveToNextLine(1))?;
-        }
-        
-        w.flush()?;
-
-        match read_char()? {
-            'q' => {
-                execute!(w, cursor::SetCursorStyle::DefaultUserShape).unwrap();
-                break;
+        match typing_synced(w, &mut buffer[i])? {
+            KeyCode::Esc => break,
+            KeyCode::Up => {
+                if i > 0 {
+                    w.execute(cursor::MoveToPreviousLine(1))?;
+                    i -= 1;
+                }
+            }
+            KeyCode::Down => {
+                if i < buffer.len() - 1 {
+                    w.execute(cursor::MoveToNextLine(1))?;
+                    i += 1;
+                }
+            }
+            KeyCode::Enter => {
+                if i == buffer.len() - 1 {
+                    buffer.push(String::new());
+                    w.execute(cursor::MoveToNextLine(1))?;
+                    i += 1;
+                }
             }
             _ => {}
         };
@@ -46,7 +79,6 @@ where W: io::Write, {
     execute!(
         w,
         style::ResetColor,
-        cursor::Show,
         terminal::LeaveAlternateScreen,
     )?;
 
@@ -54,15 +86,16 @@ where W: io::Write, {
     Ok(())
 }
 
-pub fn read_char() -> io::Result<char> {
+pub fn read_char() -> io::Result<KeyCode> {
+    use crossterm::event::{read, Event, KeyEvent, KeyEventKind};
     loop {
         if let Ok(Event::Key(KeyEvent {
-            code: KeyCode::Char(c),
+            code,
             kind: KeyEventKind::Press,
             modifiers: _,
             state: _,
-        })) = event::read() {
-            return Ok(c);
+        })) = read() {
+            return Ok(code);
         }
     }
 }
